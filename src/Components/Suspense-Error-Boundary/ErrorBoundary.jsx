534072163d7c552ed69ece51ff9cdd2e7f1ad6cf
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2026-01-16 09:20:11 +0530

    update

---
+++ src/Components/Suspense-Error-Boundary/ErrorBoundary.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export class ErrorBoundary extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {hasError: false, error: null}
+    }
+
+    static getDerivedStateFromError(error){
+        return { hasError: true, error}
+    }
+
+    componentDidCatch(error, info){
+        console.log('ErrorBoundary caught:', error, info);
+    }
+
+    handleRetry = ()=>{
+        this.setState({hasError: false, error: null})
+    };
+
+    render(){
+        if(this.state.hasError){
+            return(
+                <div>
+                    <h3>Something went wrong.</h3>
+                    <pre>
+                        {this.state.error}
+                    </pre>
+                    <button onClick={this.handleRetry}>Retry</button>
+                </div>
+            )
+        }
+        return this.prop.children;
+    }
+}
