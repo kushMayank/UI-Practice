b0d5fa10f9cf3a64194304ac85d4b8dcfba47034
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2026-02-19 13:07:42 +0530

    next text

---
+++ src/Components/UI-Interviews/NextInterviews.jsx
@@ -0,0 +1,66 @@
+
+/*
+
+Next support -
+    CSR -> client side rendering -> useEffect, browser api, events
+    SSR -> Server Side rendering -> Page generate on every request -> dynamic data dashboard
+    SSG -> Static site generation -> generate at build time -> static blog 
+    ISR -> Incremental site regeneration -> use revalidate  -> regenerate after interval -> news site
+
+
+
+    server component runs on server and reduce the JS bundle size
+
+
+
+    Data Fetching 
+
+    page based router
+    ------------------
+    1. getServerSideProps (SSR) method
+
+        export async function getServerSideProps(){
+            const res = await fetch('url');
+            const data = await res.json()
+
+            return {
+            props: {data}
+            }
+        
+        }
+
+        export default function Page({data}){
+        
+        return <div>{data.name}</data>
+        }
+
+
+    2. getStaticProps(SSG)
+        
+        export async function getStaticProps(){
+            const res = await fetch('url');
+            const data = await res.json()
+
+            return {
+                props: {data}   
+            }
+        } 
+
+
+    3 getStaticProps (ISR)
+        export async function getStaticProps(){
+            const res = await fetch('url');
+            const data = await res.json()
+
+            return {
+                props: {data},
+                revalidate: 60
+            }
+        } 
+
+    
+    App based router
+    -----------------
+        
+    
+*/
