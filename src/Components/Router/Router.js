eed6b0472345b9e10fb703cad32df3169f74b448
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2026-02-19 12:39:56 +0530

    router added

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -1,15 +1,17 @@
 
 // Basic setUp
 
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet, useNavigation} from 'react-router-dom'
+
 function App(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home/>}/>
+                <Route path='/contact' element={<Contact/>}/>
                 <Route path='/about' element={<About/>}/>
                 <Route path= '/' element={<Layout/>}>
-                    <
+                    <Route path='dashboard' element={<Dashboard/>}/>
+                    <Route path='profile' element={<Profile/>}/>
                 </Route>
             </Routes>
             
@@ -17,6 +19,54 @@ function App(){
     )
 }
 
+function Layout(){
+    return(
+        <div>
+            <Navbar/>
+            <Outlet/> {/* child router render here */}
+        </div>
+    )
+}
 
-// Nested Routing
 
+
+const navigate = useNavigation();
+navigate("/dashboard")
+navigate(-1); // back
+
+
+
+
+// Protected Route
+
+// HOC or Wrapper route to check authenticated token and redirect unauthorized
+
+<Route path='/dashboard' element={
+    <ProtectedRoute>
+        <Dashboard/>
+    </ProtectedRoute>
+}/>
+
+function ProtectedRoute({children}){
+    const isAuth = localStorage.getItem('token')
+
+    if(!isAuth){
+        return <Navigate to="/login"/>
+    }
+    return children
+}
+
+
+// Lazyloading in router
+
+/*
+import {lazy, suspense} from 'react'
+import { Suspense } from 'react';
+
+const Dashboard = lazy(()=> import('./Dashboard'))
+
+    <Suspense fallback={<p>Loading....</p>}>
+        <Dashboard/>
+    </Suspense>
+
+ */   
