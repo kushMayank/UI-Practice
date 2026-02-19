f457f861f9b26a5a9708062dcc4cb62754911efa
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2026-02-19 11:09:54 +0530

    file added

--- src/Components/Statemanagement/Statemanagement.js
+++ src/Components/Statemanagement/Statemanagement.js
@@ -14,4 +14,33 @@ createContext(with useContext)
     -> Provides a value to children without props
     -> Global/scoped to the Provider's children     
     */
-   
+
+// Use reducer
+// ----------------
+const { useReducer, createContext, useContext } = require("react");
+
+const [state, dispatch] = useReducer(reducer, initialState);
+
+function  reducer(state, action){
+    switch(action.type){
+        case 'increment':
+            return { count: state.count +1 }
+    }
+}
+
+// context API
+// ----------------
+const UserContext = createContext();
+
+export function UserProvider({children}){
+    const [user, setUser] = useState(null);
+
+    return(
+        <userContext.Provider value = {{user, setUser}}>
+            {children}
+        </userContext.Provider>
+    )
+}
+
+// use:
+const {user} = useContext(UserContext)
