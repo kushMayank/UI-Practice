b2562160eda5911cd28528aa92b591dafc67471d
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2026-02-19 11:03:09 +0530

    new file

---
+++ src/Components/Statemanagement/Statemanagement.js
@@ -0,0 +1,17 @@
+/*
+
+useReducer 
+    -> Statemenagement
+    -> Manages Complex State logic
+    -> Analogy Bank teller
+    -> Takes (state, action) and return newState
+    -> Local to the component it is defined in
+
+createContext(with useContext)
+    -> Data Sharing(Distribution)
+    -> Eliminates "props drilling"
+    -> Anology, A loudspeaker(broadcast to everyone)
+    -> Provides a value to children without props
+    -> Global/scoped to the Provider's children     
+    */
+   
